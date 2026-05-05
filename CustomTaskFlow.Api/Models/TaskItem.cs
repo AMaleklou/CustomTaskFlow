db483f88975cd0124cba8fc9e66a60baed108865
Alireza Maleklou <maleklou.a@mvmco.ir>
2026-05-05 16:40:13 +0330

    Initial Project setup

---
+++ CustomTaskFlow.Api/Models/TaskItem.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomTaskFlow.Api.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; } = null;
+        public DateTime InsertDate { get; set; }
+    }
+}
